f86249287f6f1ddd7bbca1277d50c063c22e5276
phishchiang <phishchiang@gmail.com>
2022-06-22 00:30:54 +0800

    Add the facing same direction to vertex shader

--- js/shader/vertex.glsl
+++ js/shader/vertex.glsl
@@ -12,11 +12,13 @@ varying vec3 vPosition;
 void main() {
   vUv = uv;
 
+  // vec3 instancePosi = each_instance_position * 0.1 + instance_position; // position is from PlaneBufferGeo
+
   vec4 particle_position = modelMatrix * vec4(instance_position, 1.0); // * change the size of the whole
 
   vec4 view_position = viewMatrix * particle_position + vec4(each_instance_position, 1.0) * 0.1; // * change the size of each particle
 
-  gl_Position = projectionMatrix * view_position;
+  gl_Position = projectionMatrix * view_position; // Facing to the camera
 
-  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( instancePosi, 1.0 );
+  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( instance_position + each_instance_position * 0.1, 1.0 ); // Facing the same direction
 }
